5990f983a53eafaf40ad299bc2d986d062e7c5a8
arizsalman <muhammadarizsalman@gmail.com>
2024-05-19 17:05:49 +0500

    ok

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-console.log(undefined==0);
-console.log(undefined<0);
-console.log(undefined<=0);
+// var bold_js = document.getElementById('bold')
+// var italic_js = document.getElementById('italic')
+// var underline_js = document.getElementById("underline")
+// var fontSize_js = document.getElementById('fontSize')
+// var fontFamily_js = document.getElementById('fontFamily')
+// var TextColor_js = document.getElementById('TextColor')
+ var eiditer_js =document.getElementById('eiditer')
+
+
+ function bold() {
+    eiditer_js.style.fontWeight="bold"
+ }
+ function italic() {
+    eiditer_js.style.fontStyle='italic'
+ }
+ function underline() {
+    eiditer_js.style.textDecoration='underline'
+ }
+ function fontSize() {
+    eiditer_js.style.fontSize=document.getElementById("fontSize").value
+ }
+ function fontFamily() {
+    eiditer_js.style.fontFamily='Arial'
+ }
+ 
+ function TextColor() {
+    eiditer_js.style.color= document.getElementById("changecolor").value
+ }
+ 
